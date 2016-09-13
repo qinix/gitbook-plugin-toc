1660dc95daaad7c5bd9b8802cb2744d9d25b786e
Eric Zhang <i@qinix.com>
2016-09-13 10:35:52 +0800

    update style

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var toc = require('markdown-toc');
 
 module.exports = {
-	book: {},
+	book: {
+    assets: './assets',
+    css: ['plugin.css']
+  },
 	hooks: {
 		"page:before": function (page) {
 			page.content = toc.insert(page.content, {
@@ -9,11 +12,13 @@ module.exports = {
 					return encodeURI(str.toLowerCase()).replace(/%20/g, '-');
 				}
 			});
-			if (this.options.pluginsConfig.toc.addClass) {
-				var className = this.options.pluginsConfig.toc.className || 'toc';
-				page.content = page.content + '\n\n\n<script type="text/javascript">var targetUl = document.getElementsByClassName(\'page-inner\')[0].getElementsByTagName(\'ul\')[0];if(targetUl.getElementsByTagName(\'a\').length>0){targetUl.className=\'' + className + '\';}</script>';
-			}
+
 			return page;
-		}
+		},
+    "page": function (page) {
+      page.content = page.content.replace('<!-- toc -->', '<!-- toc --><div id="toc" class="toc">\n');
+      page.content = page.content.replace('<!-- tocstop -->', '\n</div><!-- tocstop -->');
+      return page;
+    }
 	}
 };
